c633f6e9fecca0fecd5e9ad8996ceb4880d6b9e7
Bruce V. Schwartz <bruce@z2a.org>
2016-02-06 10:11:46 -0800

    checkpoint

--- README.md
+++ README.md
@@ -6,3 +6,29 @@ Built in requirements:
 * The 1000 meter event names end in "-1k"
 * The heats and lanes have already been set up
 * Column names are: Ev#,Event,Team,First_Name,Last_Name,Age,Sex,Lwt,2k Time,Heat,Time,Lane,Crash-B
+* Unused lanes are labeled "LANE n"
+
+.rac file format
+
+= Header
+LINE 0: RACE
+LINE 1: 108
+LINE 2: 0
+LINE 3: <race name, eg. "Heat9_1-10">
+LINE 4: <race distance or time in seconds, e.g. "2000", or "300">
+LINE 5: 0 <0=distance, 1=time>
+LINE 6: 0
+LINE 7: 1 <1=save stroke data>
+LINE 8: <split interval, e.g. "500">
+LINE 9: <split time, e.g. "120">
+LINE 10: <number of racers, e.g. "10">
+
+= For each lane
+LINE 0: <name>
+LINE 1: 0
+LINE 2: <blank>
+LINE 3: <country, e.g. "USA">
+LINE 4: <birth date "03041997">
+
+= Footer
+LINE: 0
--- csv2rac.js
+++ csv2rac.js
@@ -1,14 +1,21 @@
 /* jshint strict: true, node: true, multistr: true, expr: true, asi: true */
 "use strict";
 
+// CONFIG
+var outputDir = "Races"
+
+// IMPORTS
 var fs = require("fs")
+var mkdirp = require("mkdirp")
 var _ = require("lodash")
 var Converter = require("csvtojson").Converter
 var converter = new Converter({})
 
+// GLOBALS
 var csvData = fs.readFileSync("PIRC2016_HeatSheet.csv", "UTF8")
+
 converter.fromString(csvData, function(err,result) {
-    // get rid of empty lines
+    // get rid of empty lines, keep lines with an "Event"
     result = _.filter(result, "Event")
     // give each line an "event name"
     result.forEach(function(line) {
@@ -24,6 +31,11 @@ converter.fromString(csvData, function(err,result) {
     result = _.sortBy(result, [ "heatName", "Lane" ])
     // group by heat
     var events = _.groupBy(result, "heatName")
+    events = _.mapValues(events, function(heat) {
+        return _.groupBy(heat, function(line) {
+            return lanesNameFromLine(line)
+        })
+    })
     //events.forEach(function(event) {
     //    console.log("event: %s: %s rowsers", event)
     //})
@@ -33,8 +45,98 @@ converter.fromString(csvData, function(err,result) {
         console.log("heat: %s: %s rowers", name, event.length)
     })
     */
-    console.log(JSON.stringify(events, null, 4))
+    //dumpEvents(events)
+    //var raceLines = createRace(events['Heat1']['1-10'])
+    //writeRaceFile(raceLines)
 
     // each heat gets divided by lanes groups: 1-10, 11-20, 21-30, 31-40
+    _.forOwn(events, function(heat) {
+        _.forOwn(heat, function(heatLanes) {
+            var raceLines = createRace(heatLanes)
+            writeRaceFile(raceLines)
+        })
+    })
 })
 
+function lanesNameFromLine(line) {
+    if (line.Lane <= 10) return "1-10"
+    if (line.Lane <= 20) return "11-20"
+    if (line.Lane <= 30) return "21-30"
+    if (line.Lane <= 40) return "31-40"
+    error("bad line: " + JSON.stringify(line))
+}
+
+// generate the lines of an RAC file
+function createRace(race) {
+    //console.log(race)
+    var lanesName = lanesNameFromLine(race[0])
+    var raceName = "Heat" + race[0].Heat + "_" + lanesName
+    var m = lanesName.match(/(.*)-(.*)/)
+    var firstLane = parseInt(m[1], 10)
+    var lastLane = parseInt(m[2], 10)
+    console.log("creating race: " + raceName)
+    var raceLines = [
+        "RACE",
+        "108",
+        "0",
+        "<race name>",
+        "<race distance>",
+        "0",
+        "0",
+        "1",
+        "500",
+        "120",
+        "10",
+    ]
+    raceLines[3] = raceName
+    raceLines[4] = race[0].distance
+    var i = firstLane
+    _.forOwn(race, function(line) {
+        var lane = line.Lane
+        var name
+        for (var x = i; x < lane; x++) {
+            name = "Lane " + x
+            pushRace(name, x)
+        }
+        name = line.Last_Name + "," + line.First_Name
+        pushRace(name, lane)
+        i = lane + 1
+    })
+    raceLines.push("0")
+    //console.log(raceLines)
+    for (var x = i; x < lastLane; x++) {
+        var name = "Lane " + x
+        pushRace(name, x)
+    }
+
+    function pushRace(name, lane) {
+        raceLines.push(name)
+        raceLines.push("0")
+        raceLines.push("")
+        raceLines.push("")
+        raceLines.push("")
+        console.log("  %s created lane %s: %s", raceName, lane, name)
+    }
+
+    return raceLines
+}
+
+// generate the lines of an RAC file
+function writeRaceFile(raceLines) {
+    var raceName = raceLines[3]     // e.g. Heat12_11-20.rac
+    console.log(raceName)
+    var lanesName = raceName.match(/_(.*)$/)[1]
+    var dirPath = outputDir + "/" + lanesName
+    mkdirp.sync(dirPath)
+    var path = dirPath + "/" + raceName + ".rac"
+    console.log("writing %s", path)
+    fs.writeFileSync(path, raceLines.join("\r\n"))
+}
+
+function error(msg) {
+    throw new Error(msg)
+}
+
+function dumpEvents(events) {
+    console.log(JSON.stringify(events, null, 4))
+}
